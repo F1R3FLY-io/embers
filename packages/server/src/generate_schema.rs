2ebe6cadd720d212d539f0518668e23fa35a01c7
Denys Bushuliak <denys.b@devbrother.com>
2025-08-01 08:53:43 +0000

    fix:  cargo format warning

--- packages/server/src/generate_schema.rs
+++ packages/server/src/generate_schema.rs
@@ -3,7 +3,6 @@ mod common;
 mod wallets;
 
 use ai_agents::api::AIAgents;
-use poem::test::TestClient;
 use poem_openapi::OpenApiService;
 use wallets::api::WalletsApi;
 
@@ -11,5 +10,5 @@ use wallets::api::WalletsApi;
 async fn main() {
     let api = OpenApiService::new((WalletsApi, AIAgents), "Embers API", "0.1.0").url_prefix("/api");
 
-    std::fs::write("schema.json", spec.spec())
+    let _ = std::fs::write("schema.json", api.spec());
 }
