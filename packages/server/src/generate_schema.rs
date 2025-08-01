a8ca0a7a31c7400af1b906e76070e8e97da14077
Denys Bushuliak <denys.b@devbrother.com>
2025-08-01 10:41:46 +0200

    Update packages/server/src/generate_schema.rs
    Co-authored-by: IhorR-DevBrother <ihor.r@devbrother.com>

--- packages/server/src/generate_schema.rs
+++ packages/server/src/generate_schema.rs
@@ -11,20 +11,5 @@ use wallets::api::WalletsApi;
 async fn main() {
     let api = OpenApiService::new((WalletsApi, AIAgents), "Embers API", "0.1.0");
 
-    let spec = api.spec_endpoint();
-
-    fn save_to_file<T: AsRef<[u8]>>(content: T) -> std::result::Result<(), std::io::Error> {
-        std::fs::write("schema.json", content)
-    }
-
-    let _ = TestClient::new(spec)
-        .get("/schema.json")
-        .send()
-        .await
-        .0
-        .into_body()
-        .into_bytes()
-        .await
-        .map(save_to_file)
-        .unwrap();
+    std::fs::write("schema.json", spec.spec())
 }
