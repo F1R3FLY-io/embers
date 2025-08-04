79095ed08bca02e24018e6a67b047830c72cf1d4
IhorR-DevBrother <ihor.r@devbrother.com>
2025-08-04 22:16:18 +0200

    trigger ci in front with schema (#50)

--- packages/server/src/generate_schema.rs
+++ packages/server/src/generate_schema.rs
@@ -7,8 +7,7 @@ use poem_openapi::OpenApiService;
 use wallets::api::WalletsApi;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     let api = OpenApiService::new((WalletsApi, AIAgents), "Embers API", "0.1.0").url_prefix("/api");
-
-    let _ = std::fs::write("schema.json", api.spec());
+    std::fs::write("schema.json", api.spec())
 }
