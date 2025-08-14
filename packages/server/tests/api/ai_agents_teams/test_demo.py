3db38783039cc21ece72ece1e7b6de6e455cb404
ihor.r <ihor.r@devbrother.com>
2025-08-14 07:42:46 +0200

    fix lint

--- packages/server/tests/api/ai_agents_teams/test_demo.py
+++ packages/server/tests/api/ai_agents_teams/test_demo.py
@@ -2,12 +2,12 @@ from packages.server.tests.client import ApiClient
 
 
 def test_demo(client: ApiClient):
-    resp = client._http_client.post("/ai-agents-teams/deploy-demo", json={"name": "test_name"})
-    print(resp.status)
+    resp = client._http_client.post("/ai-agents-teams/deploy-demo", json={"name": "test_name"})  # noqa: SLF001
+    print(resp.status)  # noqa: T201
 
-    resp = client._http_client.post(
+    resp = client._http_client.post(  # noqa: SLF001
         "/ai-agents-teams/run-demo",
         json={"name": "test_name", "prompt": "Describe an appearance of human-like robot"},
         timeout=150,
     )
-    print(resp.body)
+    print(resp.body)  # noqa: T201
