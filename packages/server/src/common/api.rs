6e2203e0d48799275714ff60015dd33147e15dba
ihor.r <ihor.r@devbrother.com>
2025-09-23 15:58:34 +0200

    fix

--- packages/server/src/common/api.rs
+++ packages/server/src/common/api.rs
@@ -7,6 +7,7 @@ pub mod dtos;
 #[derive(Debug, Clone)]
 pub struct Service;
 
+#[allow(clippy::unused_async)]
 #[OpenApi(prefix_path = "/service", tag = ApiTags::Service)]
 impl Service {
     #[oai(path = "/ready", method = "get")]
