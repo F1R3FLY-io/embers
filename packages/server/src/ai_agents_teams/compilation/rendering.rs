fac676964706b0ef06366e0f7be4416790b4aa64
ihor.r <ihor.r@devbrother.com>
2025-09-30 09:48:43 +0200

    fix templates

--- packages/server/src/ai_agents_teams/compilation/rendering.rs
+++ packages/server/src/ai_agents_teams/compilation/rendering.rs
@@ -59,35 +59,39 @@ struct OutputTemplate<'a> {
 #[template(path = "ai_agents_teams/deploy_agent_team.rho", escape = "none")]
 struct DeployAgentTeamTemplate<'a> {
     name: &'a str,
-    channels: Vec<String>,
+    system_channels: Vec<&'static str>,
+    output_channels: Vec<String>,
     steps: Vec<String>,
     output: bool,
 }
 
-fn filter_channels<'a>(from: &[From<'a>]) -> impl Iterator<Item = &'a str> {
-    from.iter().filter_map(|f| match f {
-        From::Channel(c) => Some(*c),
-        From::Input => None,
-    })
+fn filter_channels<'a>(from: &[From<'a>]) -> Vec<&'a str> {
+    from.iter()
+        .filter_map(|f| match f {
+            From::Channel(c) => Some(*c),
+            From::Input => None,
+        })
+        .collect()
 }
 
-fn get_all_system_channels(nodes: &[&Node<'_>]) -> impl Iterator<Item = String> {
+fn get_all_system_channels(nodes: &[Node<'_>]) -> Vec<&'static str> {
     [
         nodes
             .iter()
             .any(|node| matches!(node, Node::TextModel { .. }))
-            .then_some("gpt4(`rho:ai:gpt4`)".to_owned()),
+            .then_some("gpt4(`rho:ai:gpt4`)"),
         nodes
             .iter()
             .any(|node| matches!(node, Node::TTIModel { .. }))
-            .then_some("dalle3(`rho:ai:dalle3`)".to_owned()),
+            .then_some("dalle3(`rho:ai:dalle3`)"),
         nodes
             .iter()
             .any(|node| matches!(node, Node::TTSModel { .. }))
-            .then_some("textToAudio(`rho:ai:textToAudio`)".to_owned()),
+            .then_some("textToAudio(`rho:ai:textToAudio`)"),
     ]
     .into_iter()
     .flatten()
+    .collect()
 }
 
 fn node_output_channel(index: usize) -> String {
@@ -112,11 +116,15 @@ fn get_output_for_vertex<'a, 'b>(
         .map_or(Output::Channel("devNull"), |s| Output::Channel(s))
 }
 
-#[tracing::instrument(level = "info", skip_all, fields(code), ret(Debug, level = "trace"))]
+#[tracing::instrument(
+    level = "info",
+    skip_all,
+    fields(code),
+    err(Debug),
+    ret(Debug, level = "trace")
+)]
 pub fn render_agent_team(name: &str, code: Code<'_>) -> anyhow::Result<String> {
     record_trace!(code);
-
-    let nodes: Vec<_> = code.nodes.values().collect();
 
     let vertex_outputs: BTreeMap<_, _> = code
         .nodes
@@ -132,50 +140,58 @@ pub fn render_agent_team(name: &str, code: Code<'_>) -> anyhow::Result<String> {
         .map(|v| get_input_for_vertex(&vertex_outputs, &v.from))
         .map(|from| OutputTemplate { from }.render());
 
-    let node_steps = code.nodes.iter().map(|(vertex, node)| match node {
-        Node::Compress { from, .. } => Ok(CompressTemplate {
-            from: from
-                .iter()
-                .map(|from| get_input_for_vertex(&vertex_outputs, from))
-                .collect(),
-            output: get_output_for_vertex(&vertex_outputs, vertex),
-            body: Value::Map(
-                from.iter()
-                    .map(|from| {
-                        (
-                            (*from.as_ref()).to_owned(),
-                            Inline(get_input_for_vertex(&vertex_outputs, from).to_string())
-                                .into_value(),
-                        )
-                    })
+    let steps = code
+        .nodes
+        .iter()
+        .map(|(vertex, node)| match node {
+            Node::Compress { from, .. } => Ok(CompressTemplate {
+                from: from
+                    .iter()
+                    .map(|from| get_input_for_vertex(&vertex_outputs, from))
                     .collect(),
-            ),
-        }
-        .render()?),
-        Node::TextModel { from, .. } => Ok(TextModelTemplate {
-            from: get_input_for_vertex(&vertex_outputs, from),
-            output: get_output_for_vertex(&vertex_outputs, vertex),
-        }
-        .render()?),
-        Node::TTIModel { from, .. } => Ok(TTIModelTemplate {
-            from: get_input_for_vertex(&vertex_outputs, from),
-            output: get_output_for_vertex(&vertex_outputs, vertex),
-        }
-        .render()?),
-        Node::TTSModel { from, .. } => Ok(TTSModelTemplate {
-            from: get_input_for_vertex(&vertex_outputs, from),
-            output: get_output_for_vertex(&vertex_outputs, vertex),
-        }
-        .render()?),
-    });
+                output: get_output_for_vertex(&vertex_outputs, vertex),
+                body: Value::Map(
+                    from.iter()
+                        .map(|from| {
+                            (
+                                (*from.as_ref()).to_owned(),
+                                Inline(get_input_for_vertex(&vertex_outputs, from).to_string())
+                                    .into_value(),
+                            )
+                        })
+                        .collect(),
+                ),
+            }
+            .render()?),
+            Node::TextModel { from, .. } => Ok(TextModelTemplate {
+                from: get_input_for_vertex(&vertex_outputs, from),
+                output: get_output_for_vertex(&vertex_outputs, vertex),
+            }
+            .render()?),
+            Node::TTIModel { from, .. } => Ok(TTIModelTemplate {
+                from: get_input_for_vertex(&vertex_outputs, from),
+                output: get_output_for_vertex(&vertex_outputs, vertex),
+            }
+            .render()?),
+            Node::TTSModel { from, .. } => Ok(TTSModelTemplate {
+                from: get_input_for_vertex(&vertex_outputs, from),
+                output: get_output_for_vertex(&vertex_outputs, vertex),
+            }
+            .render()?),
+        })
+        .chain(output)
+        .collect::<Result<_, _>>()?;
+
+    let output_channels = vertex_outputs.into_values().collect();
+
+    let nodes: Vec<_> = code.nodes.into_values().collect();
 
     DeployAgentTeamTemplate {
         name,
-        steps: node_steps.chain(output).collect::<Result<_, _>>()?,
+        system_channels: get_all_system_channels(&nodes),
+        output_channels,
+        steps,
         output: code.output.is_some(),
-        channels: get_all_system_channels(&nodes)
-            .chain(vertex_outputs.into_values())
-            .collect(),
     }
     .render()
     .map_err(Into::into)
